e97737e1efda1110bd5d055f430ccba4e67da34c
AktasLevent <99646648+AktasLevent@users.noreply.github.com>
2022-04-20 13:00:58 +0300

    Create aritmathics.js
    alan ve çevre

---
+++ aritmathics.js
@@ -0,0 +1,34 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    let height= 3
+    let axis= 4.5
+    area= height * axis
+  
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    let height= 3
+    let axis= 4.5 
+    perimeter= 2 *(height + axis)
+   
+    return perimeter;
